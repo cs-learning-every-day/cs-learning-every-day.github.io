82458efee917ccb1258b72de8d1dc006646d5b08
XmchxUp <XmchxUp@users.noreply.github.com>
2022-02-23 09:48:52 +0000

    deploy: 4acb2a83fb47a73a235a10d60dd9193d7da96e7f

--- en.search.min.c176b967032b58520b0c90e2ef8ef644da9a0aeaba6a7e00d4943dbedeb90ea4.js
+++ en.search.min.c176b967032b58520b0c90e2ef8ef644da9a0aeaba6a7e00d4943dbedeb90ea4.js
@@ -1,4 +1,4 @@
-'use strict';(function(){const searchDataURL='/en.search-data.min.906cb54866687dc0379e4fdd79a639d6548f2e25e57ea3c58ac11ecb62ea3fcd.json';const indexConfig=Object.assign({cache:true},{doc:{id:'id',field:['title','content'],store:['title','href','section']}});const input=document.querySelector('#book-search-input');const results=document.querySelector('#book-search-results');if(!input){return}
+'use strict';(function(){const searchDataURL='/en.search-data.min.d633eb440746621177cd1889174bb720c74368769be72e42b43ebee6b770fd86.json';const indexConfig=Object.assign({cache:true},{doc:{id:'id',field:['title','content'],store:['title','href','section']}});const input=document.querySelector('#book-search-input');const results=document.querySelector('#book-search-results');if(!input){return}
 input.addEventListener('focus',init);input.addEventListener('keyup',search);document.addEventListener('keypress',focusSearchFieldOnKeyPress);function focusSearchFieldOnKeyPress(event){if(input===document.activeElement){return;}
 const characterPressed=String.fromCharCode(event.charCode);if(!isHotkey(characterPressed)){return;}
 input.focus();event.preventDefault();}
